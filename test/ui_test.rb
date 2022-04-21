001f28ff47480c140f61a4bada8975ea9db60f3f
Anshab M K <anshabmk@live.com>
2022-04-22 00:51:28 +0530

    Remove old failing test cases

--- test/ui_test.rb
+++ test/ui_test.rb
@@ -14,36 +14,4 @@ class UITest < MiniTest::Test
 
     assert_empty(err)
   end
-
-  def test_get_user_input_exit
-    assert_raises UserInterruptException do
-      string_io = StringIO.new
-      string_io.puts "exit"
-      string_io.rewind
-      $stdin = string_io
-
-      @ui.get_user_input
-
-      $stdin = STDIN
-    end
-  end
-
-  def test_get_user_guess_co_ordinates
-    string_io = StringIO.new
-
-    string_io.puts "1,2"
-    string_io.rewind
-
-    $stdin = string_io
-
-    co_ordinates, _stdout, _stderr = OStreamCatcher.catch do
-      @ui.get_user_guess_coordinates
-    end
-
-    $stdin = STDIN
-
-    assert_instance_of(CoOrdinates, co_ordinates)
-    assert_equal(co_ordinates.x, 1)
-    assert_equal(co_ordinates.y, 2)
-  end
 end
